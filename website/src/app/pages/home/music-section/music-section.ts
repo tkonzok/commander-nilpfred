64f3bc31ab8550c1f5f05bcb7be11d8bdfc261d7
Tobias Lilienthal <tobias.lilienthalshinefour.de>
2026-06-30 17:11:23 +0200

    refactor music section, add yt player

--- website/src/app/pages/home/music-section/music-section.ts
+++ website/src/app/pages/home/music-section/music-section.ts
@@ -1,5 +1,4 @@
-import { Component, inject } from '@angular/core';
-import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, inject } from '@angular/core';
 import { AsyncPipe } from '@angular/common';
 import { DataService, Download } from '../../../shared/data.service';
 import { Observable } from 'rxjs';
@@ -8,17 +7,9 @@ import { Observable } from 'rxjs';
   selector: 'app-music-section',
   imports: [AsyncPipe],
   templateUrl: './music-section.html',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class MusicSection {
   private readonly dataService = inject(DataService);
-  private readonly sanitizer = inject(DomSanitizer);
-
   protected readonly downloads$: Observable<Download> = this.dataService.getDownloads();
-  protected readonly odesliUrl: SafeResourceUrl;
-
-  constructor() {
-    this.odesliUrl = this.sanitizer.bypassSecurityTrustResourceUrl(
-      'https://odesli.co/embed/?url=https%3A%2F%2Falbum.link%2Fi%2F1587016663&theme=light',
-    );
-  }
 }
